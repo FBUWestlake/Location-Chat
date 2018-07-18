1d59310d6ef74683ef03c479b1a2e8bd855a6ebf
Jacob Snyder <jsnyder@fb.com>
2018-07-18 11:03:09 -0700

    Small fixes with LoginActivity and ChatActivity together
    Changed startup activity to LoginActivity
    Disabled Parse login feature to prevent app crash
    'Log In' button switches to ChatActivity
    Removed redundancy in dimens.xml

--- app/src/main/java/me/susiel2/locationchat/LoginActivity.java
+++ app/src/main/java/me/susiel2/locationchat/LoginActivity.java
@@ -1,16 +1,12 @@
 package me.susiel2.locationchat;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.parse.LogInCallback;
-import com.parse.ParseException;
 import com.parse.ParseUser;
 
 
@@ -27,10 +23,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ParseUser currentUser = ParseUser.getCurrentUser();
+        // TODO - set up parse login
+//        ParseUser currentUser = ParseUser.getCurrentUser();
+        ParseUser currentUser = null;
+
         if (currentUser != null) {
             //HomeActivity is the main screen of the app, where we want users to go after logging in
-            final Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            final Intent intent = new Intent(LoginActivity.this, ChatActivity.class);
             startActivity(intent);
             finish();
         }
@@ -60,22 +59,28 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login(String username, String password) {
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                if (e == null) {
-                    Log.d("LoginActivity", "Login successful");
 
-                    //HomeActivity is the main screen of the app, where we want users to go after logging in
-                    final Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Log.e("LoginActivity", "Login failure");
-                    e.printStackTrace();
-                }
-            }
-        });
+        final Intent intent = new Intent(LoginActivity.this, ChatActivity.class);
+        startActivity(intent);
+        finish();
+
+        // TODO - set up parse login
+//        ParseUser.logInInBackground(username, password, new LogInCallback() {
+//            @Override
+//            public void done(ParseUser user, ParseException e) {
+//                if (e == null) {
+//                    Log.d("LoginActivity", "Login successful");
+//
+//                    //HomeActivity is the main screen of the app, where we want users to go after logging in
+//                    final Intent intent = new Intent(LoginActivity.this, ChatActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                } else {
+//                    Log.e("LoginActivity", "Login failure");
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
     }
 
 }
