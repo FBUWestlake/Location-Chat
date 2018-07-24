97eb11a430c51f1318529c0511b1cfac78d15f5b
Jacob Snyder <jsnyder@fb.com>
2018-07-24 15:26:54 -0700

    Started converting Chat and Message classes to ParseObject classes
    NOT A WORKING VERSION. WIP

--- app/src/main/java/me/susiel2/locationchat/model/Chat.java
+++ app/src/main/java/me/susiel2/locationchat/model/Chat.java
@@ -2,112 +2,89 @@ package me.susiel2.locationchat.model;
 
 import android.graphics.Bitmap;
 
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 import org.parceler.Parcel;
 
 import java.io.File;
 
-@Parcel
-public class Chat {
+@ParseClassName("Group")
+public class Chat extends ParseObject {
 
-    public String name;
-    public String description;
-    public int numberOfMembers;
-    public String imageUrl;
-    public Bitmap bitmapImage;
-    public String category;
-    public File imageFile;
+    private static final String KEY_NAME = "name";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_IMAGE = "image";
+    private static final String KEY_CATEGORY = "category";
+    private static final String KEY_CREATED_BY = "createdBy";
+    private static final String KEY_LOCATION_ID = "location";
 
-    public Chat(){
-        name = "";
-        numberOfMembers = 0;
-        imageUrl = "";
-        category = "";
-        description = "";
-        bitmapImage = null;
+    public String getGroupIdString() {
+        return getObjectId().toString();
     }
-
-    public Chat(String name, String chatImage, String description, String category, int numberOfMembers) {
-        this.name = name;
-        this.imageUrl = chatImage;
-        this.description = description;
-        this.category = category;
-        this.numberOfMembers = numberOfMembers;
-    }
-
-    public Chat(String name, Bitmap bitmapImage, String description, String category, int numberOfMembers) {
-        this.name = name;
-        this.bitmapImage = bitmapImage;
-        this.description = description;
-        this.category = category;
-        this.numberOfMembers = numberOfMembers;
-    }
-
-    public Chat(String name, File imageFile, String description, String category, int numberOfMembers) {
-        this.name = name;
-        this.imageFile = imageFile;
-        this.description = description;
-        this.category = category;
-        this.numberOfMembers = numberOfMembers;
-    }
-
 
     public String getName() {
-        return name;
+        return getString(KEY_NAME);
     }
 
     public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getNumberOfMembers() {
-        return numberOfMembers;
-    }
-
-    public void setNumberOfMembers(int numberOfMembers) {
-        this.numberOfMembers = numberOfMembers;
-    }
-
-    public void incrementNumberOfMembers(){
-        numberOfMembers++;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
+        put(KEY_NAME, name);
     }
 
     public String getDescription() {
-        return description;
+        return getString(KEY_DESCRIPTION);
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String desc) {
+        put(KEY_DESCRIPTION, desc);
     }
 
-    public Bitmap getBitmapImage() {
-        return bitmapImage;
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
     }
 
-    public void setBitmapImage(Bitmap bitmapImage) {
-        this.bitmapImage = bitmapImage;
+    public void setImage(ParseFile obj) {
+        put(KEY_IMAGE, obj);
     }
 
-    public File getImageFile() {
-        return imageFile;
+    public String getCategory() {
+        return getString(KEY_CATEGORY);
     }
 
-    public void setImageFile(File image) {
-        this.imageFile = imageFile;
+    public void setCategory(String category) {
+        put(KEY_CATEGORY, category);
     }
+
+    public ParseUser getCreatedBy() {
+        return getParseUser(KEY_CREATED_BY);
+    }
+
+    public void setCreatedBy(ParseUser user) {
+        put(KEY_CREATED_BY, user);
+    }
+
+    public int getLocation() {
+        return getInt(KEY_LOCATION_ID);
+    }
+
+    public void setLocation(int location) {
+        put(KEY_LOCATION_ID, location);
+    }
+
+    public String getCreatedAtString() {
+        return getCreatedAt().toString();
+    }
+
+    public static class Query extends ParseQuery<Chat> {
+        public Query() {
+            super(Chat.class);
+        }
+
+        // TODO - add useful Query methods
+
+    }
+
 }
--- app/src/main/java/me/susiel2/locationchat/model/Message.java
+++ app/src/main/java/me/susiel2/locationchat/model/Message.java
@@ -1,50 +1,80 @@
 package me.susiel2.locationchat.model;
 
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+@ParseClassName("Message")
 public class Message {
 
-    public String message;
-    public String user;
-    public String time;
-    public String userImage;
+    private static final String KEY_CONTENT = "content";
+    private static final String KEY_ATTACHMENT = "attachment";
+    private static final String KEY_GROUP_ID = "groupId";
+    private static final String KEY_CREATED_BY = "createdBy";
 
-    public Message(String message, String user, String time, String userImage){
-        this.message = message;
-        this.user = user;
-        this.time = time;
-        this.userImage = userImage;
+    public ParseObject getGroupIdString() {
+        return getObjectId().toString();
     }
 
-    // TODO - add method for parsing raw createdAt() data
-
-    public String getMessage() {
-        return message;
+    public String getName() {
+        return getString(KEY_NAME);
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setName(String name) {
+        put(KEY_NAME, name);
     }
 
-    public String getUser() {
-        return user;
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
     }
 
-    public void setUser(String user) {
-        this.user = user;
+    public void setDescription(String desc) {
+        put(KEY_DESCRIPTION, desc);
     }
 
-    public String getTime() {
-        return time;
+    public ParseObject getImage() {
+        return getParseObject(KEY_IMAGE);
     }
 
-    public void setTime(String time) {
-        this.time = time;
+    public void setImage(ParseObject obj) {
+        put(KEY_IMAGE, obj);
     }
 
-    public String getUserImage() {
-        return userImage;
+    public String getCategory() {
+        return getString(KEY_CATEGORY);
     }
 
-    public void setUserImage(String userImage) {
-        this.userImage = userImage;
+    public void setCategory(String category) {
+        put(KEY_CATEGORY, category);
+    }
+
+    public ParseUser getCreatedBy() {
+        return getParseUser(KEY_CREATED_BY);
+    }
+
+    public void setCreatedBy(ParseUser user) {
+        put(KEY_CREATED_BY, user);
+    }
+
+    public int getLocation() {
+        return getInt(KEY_LOCATION_ID);
+    }
+
+    public void setLocation(int location) {
+        put(KEY_LOCATION_ID, location);
+    }
+
+    public String getCreatedAtString() {
+        return getCreatedAt().toString();
+    }
+
+    public static class Query extends ParseQuery<Chat> {
+        public Query() {
+            super(Chat.class);
+        }
+
+        // TODO - add useful Query methods
+
     }
 }
