8983c57b25c423307dd21aded2c647f316122fa1
Susie Lee <leesusie@fb.com>
2018-07-23 17:12:57 -0700

    add updated database tables

--- app/src/main/java/me/susiel2/locationchat/database/DatabaseHelper.java
+++ app/src/main/java/me/susiel2/locationchat/database/DatabaseHelper.java
@@ -7,8 +7,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "bounce.db";
-    public static final String TABLE_ONE_NAME = "users_table";
-    public static final String TABLE_TWO_NAME = "groups_table";
+    public static final String TABLE_ONE_NAME = "users";
+    public static final String TABLE_TWO_NAME = "groups";
+    public static final String TABLE_THREE_NAME = "messages";
+    public static final String TABLE_FOUR_NAME = "location";
+    public static final String TABLE_FIVE_NAME = "user_groups";
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -17,14 +21,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table " + TABLE_ONE_NAME + " (NAME TEXT PRIMARY KEY, PHONE_NUMBER INTEGER, EMAIL TEXT, PASSWORD TEXT, GROUPS TEXT, CATEGORIES TEXT)");
-        sqLiteDatabase.execSQL("create table " + TABLE_TWO_NAME + "(NAME TEXT PRIMARY KEY, DESCRIPTION TEXT, NUMBER_OF_MEMBERS INTEGER, CATEGORY TEXT, CREATED_AT TEXT)");
+        sqLiteDatabase.execSQL("create table " + TABLE_ONE_NAME + " (USER_ID TEXT PRIMARY KEY, NAME TEXT, PHONE_NUMBER INTEGER, PASSWORD TEXT, CREATED_AT TEXT, LOCATION_ID INTEGER, FOREIGN KEY(LOCATION_ID) REFERENCES location(LOCATION_ID))");
+        sqLiteDatabase.execSQL("create table " + TABLE_TWO_NAME + "(GROUP_ID TEXT PRIMARY KEY, NAME TEXT, DESCRIPTION TEXT, PHOTO BLOB, CREATED_AT TEXT, CREATED_BY TEXT, LOCATION_ID INTEGER, FOREIGN KEY(CREATED_BY) REFERENCES users(USER_ID), FOREIGN KEY(LOCATION_ID) REFERENCES location(LOCATION_ID))");
+        sqLiteDatabase.execSQL("create table " + TABLE_THREE_NAME + "(MESSAGE_ID TEXT PRIMARY KEY, CONTENT TEXT, CREATED_AT TEXT, ATTACHMENT BLOB, CREATED_BY TEXT, GROUP_ID TEXT, FOREIGN KEY(CREATED_BY) REFERENCES user(USER_ID), FOREIGN KEY(GROUP_ID) REFERENCES groups(GROUP_ID))");
+        sqLiteDatabase.execSQL("create table " + TABLE_FOUR_NAME + "(LOCATION_ID INTEGER PRIMARY KEY, LOCATION TEXT)");
+        sqLiteDatabase.execSQL("create table " + TABLE_FIVE_NAME + "(NOTIFICATIONS INTEGER, READ INTEGER, USER_ID TEXT, GROUP_ID TEXT, FOREIGN KEY(USER_ID) REFERENCES users(USER_ID), FOREIGN KEY(GROUP_ID) REFERENCES groups(GROUP_ID))");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int j) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_ONE_NAME);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_TWO_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_THREE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_FOUR_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_FIVE_NAME);
+
         onCreate(sqLiteDatabase);
     }
 }
