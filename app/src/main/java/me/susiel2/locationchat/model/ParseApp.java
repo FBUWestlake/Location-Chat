00d49e07d31afb252f8f6c3d5490d7c8fc6ee49c
Jacob Snyder <jsnyder@fb.com>
2018-07-23 18:30:14 -0700

    Parse Server code

---
+++ app/src/main/java/me/susiel2/locationchat/model/ParseApp.java
@@ -0,0 +1,23 @@
+package me.susiel2.locationchat.model;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
+                .applicationId("bounceId")
+                .clientKey("bounceKey")
+                .server("http://bounce-fbu.herokuapp.com/parse")
+                .build();
+        Parse.initialize(configuration);
+    }
+
+}
