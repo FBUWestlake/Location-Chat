9412a58e04774e809103e42b9694a4836cb620da
Susie Lee <leesusie@fb.com>
2018-07-25 14:36:24 -0700

    move parse operation methods to new parse operations class

--- app/src/main/java/me/susiel2/locationchat/database/ParseOperations.java
+++ app/src/main/java/me/susiel2/locationchat/database/ParseOperations.java
@@ -1,23 +1,32 @@
 package me.susiel2.locationchat.database;
 
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import me.susiel2.locationchat.ChatActivity;
+import me.susiel2.locationchat.model.Message;
+import me.susiel2.locationchat.model.MessageAdapter;
 
 public class ParseOperations {
 
     // Enjoy. :^)
+    Message message;
 
-    public void createMessage(EditText messageContent) {
-        String content = messageContent.getText().toString();
+    public void createMessage(String content) {
         ParseObject message = ParseObject.create("Message");
-        //content.put("USER_ID", ParseUser.getCurrentUser().getObjectId());
+        message.put("USER_ID", ParseUser.getCurrentUser().getObjectId());
         message.put("content", content);
         // TODO: message.put("groupId", groupId);
         message.saveInBackground(new SaveCallback() {
@@ -31,6 +40,24 @@ public class ParseOperations {
                 }
             }
         });
-        messageContent.setText(null);
     }
+
+    // Functions to get message information.
+
+    public String getMessageContent() {
+        return message.getContent();
+    }
+
+    public String getMessageCreatorName() {
+        return message.getCreatedBy().getString("name");
+    }
+
+    public ParseUser getMessageCreatorUser() {
+        return message.getCreatedBy();
+    }
+
+    public String getMessageCreatedTime() {
+        return message.getCreatedAtString();
+    }
+
 }
