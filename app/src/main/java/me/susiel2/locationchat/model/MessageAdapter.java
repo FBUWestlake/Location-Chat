43ac3407db4c1158d45478e2e1cebbbb534c60a8
Jacob Snyder <jsnyder@fb.com>
2018-08-13 11:23:14 -0700

    Fixed bug where poster's name and attached image is not bound if score is too low
    Previous fix didn't work

--- app/src/main/java/me/susiel2/locationchat/model/MessageAdapter.java
+++ app/src/main/java/me/susiel2/locationchat/model/MessageAdapter.java
@@ -235,41 +235,41 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
             } else {
                 messageText.setText(message.getContent());
+            }
 
-                ParseQuery<ParseUser> query = ParseQuery.getQuery(ParseUser.class);
-                query.whereEqualTo("objectId", message.getCreatedBy().getObjectId());
+            ParseQuery<ParseUser> query = ParseQuery.getQuery(ParseUser.class);
+            query.whereEqualTo("objectId", message.getCreatedBy().getObjectId());
 
-                query.findInBackground(new FindCallback<ParseUser>() {
-                    public void done(List<ParseUser> objects, ParseException e) {
-                        if (e == null) {
-                            if (objects.size() != 0)
-                                nameText.setText(objects.get(0).getString("name"));
-                            else
-                                nameText.setText("[deleted]");
-                        } else {
-                            // Something went wrong.
-                        }
+            query.findInBackground(new FindCallback<ParseUser>() {
+                public void done(List<ParseUser> objects, ParseException e) {
+                    if (e == null) {
+                        if (objects.size() != 0)
+                            nameText.setText(objects.get(0).getString("name"));
+                        else
+                            nameText.setText("[deleted]");
+                    } else {
+                        // Something went wrong.
                     }
-                });
-
-
-                if (message.getFile() != null) {
-                    Log.e("MessageAdapter", "binding image to message " + message.getContent() + " and file " + message.getFile());
-                    Bitmap bm_resized = null;
-                    try {
-                        String filePath = message.getFile().getFile().getAbsolutePath();
-                        bm_resized = BitmapScaler.scaleToFitWidth(BitmapFactory.decodeFile(filePath), 500);
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                    bm_resized.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
-                    Glide.with(context).load(bm_resized).apply(RequestOptions.bitmapTransform(new RoundedCornersTransformation(25, 0, RoundedCornersTransformation.CornerType.ALL)))
-                            .into(attachedImage);
-                } else {
-                    Drawable myDrawable = context.getResources().getDrawable(R.drawable.asfalt_light);
-                    attachedImage.setImageDrawable(myDrawable);
                 }
+            });
+
+
+            if (message.getFile() != null) {
+                Log.e("MessageAdapter", "binding image to message " + message.getContent() + " and file " + message.getFile());
+                Bitmap bm_resized = null;
+                try {
+                    String filePath = message.getFile().getFile().getAbsolutePath();
+                    bm_resized = BitmapScaler.scaleToFitWidth(BitmapFactory.decodeFile(filePath), 500);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                bm_resized.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+                Glide.with(context).load(bm_resized).apply(RequestOptions.bitmapTransform(new RoundedCornersTransformation(25, 0, RoundedCornersTransformation.CornerType.ALL)))
+                        .into(attachedImage);
+            } else {
+                Drawable myDrawable = context.getResources().getDrawable(R.drawable.asfalt_light);
+                attachedImage.setImageDrawable(myDrawable);
             }
 
             timeText.setText(message.getCreatedAtString());
